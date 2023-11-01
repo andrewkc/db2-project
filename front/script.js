aec59e597144bd964b4ffc78b509329e33959e37
Johan <johan.callinapa@utec.edu.pe>
2023-10-31 23:32:18 -0500

    update

--- front/script.js
+++ front/script.js
@@ -1,36 +1,98 @@
+let imageMap = {};
+
+async function loadImageMap() {
+    try {
+        // Reemplaza con tu URL "raw" de GitHub
+        const response = await fetch('https://raw.githubusercontent.com/johancalli/test/main/images.csv'); 
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const csvText = await response.text();
+        console.log('CSV Text:', csvText);  // Agregado para depuración
+        const rows = csvText.split('\n');
+        rows.shift(); // Eliminar la primera fila (encabezados)
+        rows.forEach(row => {
+            const [filename, link] = row.split(',');
+            const id = parseInt(filename.split('.')[0]); 
+            imageMap[id] = link;
+        });
+        console.log('Image map loaded:', imageMap);  // Agregado para depuración
+    } catch (error) {
+        console.error('Ha ocurrido un error al cargar el CSV:', error);
+    }
+}
+
+
+window.addEventListener('load', () => {
+    loadImageMap();
+});
+
+const BASE_URL = "http://127.0.0.1:8000";
+
+// Función para obtener datos desde la API y mostrarlos
 async function fetchData() {
-    const query = document.getElementById("queryInput").value;
-    const k = parseInt(document.getElementById("kInput").value);
-    const method = document.getElementById("methodSelector").value;
+    const query = document.getElementById('queryInput').value;
+    const k = parseInt(document.getElementById('kInput').value); // Convertir a entero
+    const method = document.getElementById('methodSelector').value;
+
+    if (!query || isNaN(k)) {
+        alert("Por favor, completa todos los campos.");
+        return;
+    }
+
+    const apiUrl = `${BASE_URL}${method}`;
+    const payload = {
+        query: query,
+        k: k
+    };
 
     try {
-        const response = await fetch(method, {
+        const response = await fetch(apiUrl, {
             method: 'POST',
-            body: JSON.stringify({ query: query, k: k }),
             headers: {
-                'Content-Type': 'application/json'
-            }
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
         });
 
-        const data = await response.json();
-        document.getElementById("data").innerText = JSON.stringify(data.content, null, 2);
+        if (response.ok) {
+            const data = await response.json();
+            const resultContainer = document.getElementById('resultContainer');
+            resultContainer.innerHTML = ''; // Limpiar resultados anteriores
+            
+            let displayedResults = 0; // Contador para saber cuántos resultados se han mostrado
 
-    } catch (error) {
-        console.error("Hubo un error al recuperar los datos:", error);
-    }
-}
+            data.content.forEach(([id, description]) => {
+                if (displayedResults >= k) {
+                    return; // Detener el loop si ya hemos mostrado k resultados
+                }
 
-async function createIndex() {
-    try {
-        const response = await fetch('/create_index', { method: 'POST' });
-        const result = await response.json();
+                const imageLink = imageMap[id];
+                const resultItem = document.createElement('div');
+                resultItem.className = 'result-item';
 
-        if (result.response === 200) {
-            alert("Índice creado exitosamente.");
+                if (imageLink) {
+                    const imgElement = document.createElement('img');
+                    imgElement.src = imageLink;
+                    imgElement.alt = `Imagen de ID ${id}`;
+                    imgElement.style.maxWidth = "200px";  // Redimensionar la imagen si es necesario
+                    imgElement.style.maxHeight = "200px";  // Redimensionar la imagen si es necesario
+                    resultItem.appendChild(imgElement);
+                }
+
+                const descriptionElement = document.createElement('p');
+                descriptionElement.textContent = description;
+                resultItem.appendChild(descriptionElement);
+
+                resultContainer.appendChild(resultItem);
+                
+                displayedResults++;  // Incrementar el contador de resultados mostrados
+            });
         } else {
-            alert("Error al crear el índice.");
+            alert('Error al realizar la consulta');
         }
     } catch (error) {
-        console.error("Hubo un error al crear el índice:", error);
+        console.error('Hubo un problema con la operación fetch:', error);
     }
 }
+
